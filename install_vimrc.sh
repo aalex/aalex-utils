4c7bf3deed1a63cf6ce7f12477fd8bd683292d71
Alexandre Quessy <alexandre@quessy.net>
2019-10-17 16:49:59 -0400

    add vcscommand plugin for Vim

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd $(dirname $0)
 cat vimrc >> ~/.vimrc
-mkdir -p ~/.vim/syntax
+mkdir -p ~/.vim/syntax/
+mkdir -p ~/.vim/plugin/
+mkdir -p ~/.vim/doc/
 cp vim/syntax/* ~/.vim/syntax/
+
+# Install vcscommand
+pushd vcscommand/
+unzip vcscommand-1.99.47.zip
+cp plugin/* ~/.vim/plugin/
+cp doc/* ~/.vim/doc/
+cp syntax/* ~/.vim/syntax/
+popd
